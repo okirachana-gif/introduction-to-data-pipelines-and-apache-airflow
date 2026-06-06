6b8c0a57d3748f8a8969b9254ddc891e77f49ef0
okirachana-gif <oki.rachana@gmail.com>
2026-06-06 04:48:50 +0000

    Write my first program in Python

---
+++ my_first_program.py
@@ -0,0 +1,36 @@
+# print("Hello world")
+print("Data pipeline")
+
+#ตัวแปร
+name = "Oki Rachana"
+number = 10
+a = 5.5
+b = 9
+print(name, number, a, b)
+
+# Operation
+c = a + b
+c = c * 10
+print(c)
+
+print (5 % 4)
+print (5 % 2)
+
+# Conditional
+if c > 150:
+    c = 100
+    a = 100
+    b = 100
+else:
+    c = 9
+    a = 9
+    b = 9
+
+print(a, b, c)
+
+# loop
+l = [1, 2, 3, 4, 5]
+for each_item in l:
+    if each_item % 2 == 0:
+        print(each_item)
+
